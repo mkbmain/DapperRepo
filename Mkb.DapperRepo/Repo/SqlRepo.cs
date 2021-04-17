c8ff350c1ba875f44bed498e433eb4d067b3f174
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-04-17 03:41:25 +0100

    V1.0.3 Made all method virtual for easier mocks

--- Mkb.DapperRepo/Repo/SqlRepo.cs
+++ Mkb.DapperRepo/Repo/SqlRepo.cs
@@ -11,45 +11,45 @@ namespace Mkb.DapperRepo.Repo
         }
 
 
-        public T QuerySingle<T>(string sql)
+        public virtual T QuerySingle<T>(string sql)
         {
             return BaseGetAll((connection, sql2) => connection.QueryFirstOrDefault<T>(sql2), sql);
         }
 
-        public IEnumerable<T> QueryMany<T>(string sql)
+        public virtual IEnumerable<T> QueryMany<T>(string sql)
         {
             return BaseGetAll((connection, sql2) => connection.Query<T>(sql2), sql);
         }
 
-        public T GetById<T>(T element)
+        public virtual T GetById<T>(T element)
         {
             return BaseGet<T, T>((connection, s) => connection.QueryFirstOrDefault<T>(s, element));
         }
 
-        public IEnumerable<T> GetAll<T>()
+        public virtual IEnumerable<T> GetAll<T>()
         {
             return BaseGetAll<T, IEnumerable<T>>((connection, s) => connection.Query<T>(s));
         }
 
-        public IEnumerable<T> Search<T>(string property, string term) where T : class, new()
+        public virtual IEnumerable<T> Search<T>(string property, string term) where T : class, new()
         {
             T item = new T();
             return Search(item, property, term);
         }
 
-        public IEnumerable<T> Search<T>(T item, string property, string term)
+        public virtual IEnumerable<T> Search<T>(T item, string property, string term)
         {
             var theField = ReflectionUtils.GetPropertyInfoOfType<T>(typeof(string), property);
             theField.SetValue(item, term);
             return BaseSearch<T, IEnumerable<T>>((connection, s) => connection.Query<T>(s, item), property);
         }
 
-        public T Add<T>(T element)
+        public virtual T Add<T>(T element)
         {
             return BaseAdd(new[] {element}, (connection, s) => connection.QuerySingle<T>(s, element), true);
         }
 
-        public void AddMany<T>(IEnumerable<T> elements)
+        public virtual void AddMany<T>(IEnumerable<T> elements)
         {
             BaseAdd(elements, (connection, s) =>
             {
@@ -58,7 +58,7 @@ namespace Mkb.DapperRepo.Repo
             }, false);
         }
 
-        public void Update<T>(T element, bool ignoreNullProperties = false)
+        public virtual void Update<T>(T element, bool ignoreNullProperties = false)
         {
             BaseUpdate(element, ignoreNullProperties, (connection, s) =>
             {
@@ -67,7 +67,7 @@ namespace Mkb.DapperRepo.Repo
             });
         }
 
-        public void Delete<T>(T element)
+        public virtual void Delete<T>(T element)
         {
             BaseDelete<T>((connection, s) =>
             {
--- Mkb.DapperRepo/Repo/SqlRepoAsync.cs
+++ Mkb.DapperRepo/Repo/SqlRepoAsync.cs
@@ -12,33 +12,33 @@ namespace Mkb.DapperRepo.Repo
         }
         
 
-        public Task<T> QuerySingle<T>(string sql)
+        public virtual Task<T> QuerySingle<T>(string sql)
         {
             return BaseGetAll((connection, sql2) => connection.QueryFirstOrDefaultAsync<T>(sql2), sql);
         }
 
-        public Task<IEnumerable<T>> QueryMany<T>(string sql)
+        public virtual Task<IEnumerable<T>> QueryMany<T>(string sql)
         {
             return BaseGetAll((connection, sql2) => connection.QueryAsync<T>(sql2), sql);
         }
 
-        public Task<T> GetById<T>(T element)
+        public virtual Task<T> GetById<T>(T element)
         {
             return BaseGet<T, Task<T>>((connection, s) => connection.QueryFirstOrDefaultAsync<T>(s, element));
         }
 
-        public Task<IEnumerable<T>> GetAll<T>()
+        public virtual Task<IEnumerable<T>> GetAll<T>()
         {
             return BaseGetAll<T, Task<IEnumerable<T>>>((connection, s) => (connection.QueryAsync<T>(s)));
         }
         
-        public Task<IEnumerable<T>> Search<T>(string property, string term) where T : class, new()
+        public virtual Task<IEnumerable<T>> Search<T>(string property, string term) where T : class, new()
         {
             T item = new T();
             return Search(item, property, term);
         }
 
-        public Task<IEnumerable<T>> Search<T>(T item, string property, string term) 
+        public virtual Task<IEnumerable<T>> Search<T>(T item, string property, string term) 
         {
             var theField = ReflectionUtils.GetPropertyInfoOfType<T>(typeof(string), property);
 
@@ -47,24 +47,24 @@ namespace Mkb.DapperRepo.Repo
             return BaseSearch<T, Task<IEnumerable<T>>>((connection, s) => connection.QueryAsync<T>(s, item), property);
         }
 
-        public Task<T> Add<T>(T element)
+        public virtual Task<T> Add<T>(T element)
         {
             return BaseAdd(new[] {element},
                 async (connection, s) => (await connection.QueryAsync<T>(s, element)).First(), true);
         }
 
-        public Task AddMany<T>(IEnumerable<T> elements)
+        public virtual Task AddMany<T>(IEnumerable<T> elements)
         {
             return BaseAdd<T, Task>(elements, (connection, s) => connection.ExecuteAsync(s, elements), false);
         }
 
-        public Task Update<T>(T element, bool ignoreNullProperties = false)
+        public virtual Task Update<T>(T element, bool ignoreNullProperties = false)
         {
             return BaseUpdate(element, ignoreNullProperties,
                 (connection, s) => connection.ExecuteAsync(s, new[] {element}));
         }
 
-        public Task Delete<T>(T element)
+        public virtual Task Delete<T>(T element)
         {
             return BaseDelete<T>((connection, s) => connection.ExecuteAsync(s, new[] {element}));
         }
