6d06d1cbb51b8d82225f581af83659e1ebf035ed
mkbmain <michael.bourke@live.co.uk>
2022-03-12 21:19:43 +0000

    tidy up

--- Mkb.DapperRepo/Search/SearchCriteria.cs
+++ Mkb.DapperRepo/Search/SearchCriteria.cs
@@ -2,20 +2,9 @@ using System;
 
 namespace Mkb.DapperRepo.Search
 {
-
     public class SearchCriteria
     {
         public string PropertyName { get; set; }
         public SearchType SearchType { get; set; }
-    }
-
-    public enum SearchType
-    {
-        Equals,
-        Like,
-        GreaterThan,
-        LessThan,
-        GreaterThanEqualTo,
-        LessThanEqualTo
     }
 }
