bc4cd74f7f2fa32a5cc03aabc86f639494f8f972
mkbmain <michael.bourke@live.co.uk>
2022-03-12 21:19:27 +0000

    not needed

--- Mkb.DapperRepo/Search/SearchCriteria.cs
+++ Mkb.DapperRepo/Search/SearchCriteria.cs
@@ -2,11 +2,6 @@ using System;
 
 namespace Mkb.DapperRepo.Search
 {
-    public class SearchCriteriaValue : SearchCriteria
-    {
-        public object Value { get; set; }
-        public Type PropertyType { get; set; }
-    }
 
     public class SearchCriteria
     {
