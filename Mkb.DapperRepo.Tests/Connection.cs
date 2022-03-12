f3008dfd3f027a93e0486846997c58d5d23b2062
mkbmain <michael.bourke@live.co.uk>
2022-03-12 21:21:35 +0000

    restored

--- Mkb.DapperRepo.Tests/Connection.cs
+++ Mkb.DapperRepo.Tests/Connection.cs
@@ -2,6 +2,6 @@ namespace Mkb.DapperRepo.Tests
 {
     public static class Connection
     {
-        public const string MasterConnectionString = "Server=localhost;Database=master;User Id=sa;Password=A1234567a;";
+        public const string MasterConnectionString = "Server=192.168.0.216;Database=master;User Id=sa;Password=12345678;";
     }
 }
