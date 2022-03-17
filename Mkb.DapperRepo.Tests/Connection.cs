e1847eeeb5ec0c050ee30193c18a1af52ab7bf5c
mkbmain <michael.bourke@live.co.uk>
2022-03-17 09:52:15 +0000

    Tests for Sqlite

--- Mkb.DapperRepo.Tests/Connection.cs
+++ Mkb.DapperRepo.Tests/Connection.cs
@@ -1,16 +1,19 @@
 namespace Mkb.DapperRepo.Tests
 {
     public static class Connection
-    {
-       //  public const string MasterConnectionString = "Server=localhost;Database=master;User Id=sa;Password=A1234567a;";  // SQL
-       public const string MasterConnectionString = "Server=localhost;Database=master;Uid=root;Pwd=A1234567a;"; // MYSQL
-      //  public const string MasterConnectionString = "User ID=postgres;Password=A1234567a;Host=localhost;Port=5432;Database=master;"; // Postgres
-        public static Enviroment SelectedEnvironment = Enviroment.MySql;
+    { 
+     // public const string MasterConnectionString = "Server=localhost;Database=master;User Id=sa;Password=A1234567a;";  // SQL
+      //public const string MasterConnectionString = "Server=localhost;Database=master;Uid=root;Pwd=A1234567a;"; // MYSQL
+      //  public const string MasterConnectionString = "Server=192.168.0.216;Database=master;Uid=sa;Pwd=A1234567a;"; // MYSQL
+   //  public const string MasterConnectionString = "User ID=postgres;Password=A1234567a;Host=localhost;Port=5432;Database=master;"; // Postgres
+     public const string MasterConnectionString = "Data Source=master.sqlite"; // SqlLite
+        public static Enviroment SelectedEnvironment = Enviroment.SqlLite;
     }
 
     public enum Enviroment
     {
         Sql,
+        SqlLite,
         MySql,
         PostgreSQL
     }
