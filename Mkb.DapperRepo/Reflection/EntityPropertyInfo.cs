63dd482eedf18466d94927e53e43fb2d1717304b
mkbmain <michael.bourke@live.co.uk>
2022-08-24 20:00:34 +0100

    bit more ram usage but lot let reflections .. types do not change during runtime so makes sense

--- Mkb.DapperRepo/Reflection/EntityPropertyInfo.cs
+++ Mkb.DapperRepo/Reflection/EntityPropertyInfo.cs
@@ -1,20 +1,39 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Mkb.DapperRepo.Tests")]
+
 namespace Mkb.DapperRepo.Reflection
 {
     internal class EntityPropertyInfo
     {
+        private Dictionary<string, PropertyInfo[]> QuickNameLookUp;
+
         public EntityPropertyInfo(PropertyInfo id, IEnumerable<PropertyInfo> all)
         {
             Id = id;
             All = all;
+            QuickNameLookUp = all.GroupBy(e => e.Name.ToLower()).ToDictionary(e => e.Key, e => e.ToArray());
         }
+
+        public PropertyInfo NameLookUp(string name, Type type) => NameLookUp(name)?.FirstOrDefault(e => e.PropertyType == type);
+        
+
+        public IEnumerable<PropertyInfo> NameLookUp(string name)
+        {
+            if (QuickNameLookUp.TryGetValue(name.ToLower(), out var items))
+            {
+                return items;
+            }
+
+            return null;
+        }
+
         public PropertyInfo Id { get; set; }
         internal IEnumerable<PropertyInfo> All { get; set; }
-        public IEnumerable<PropertyInfo> AllNonId => All.Where(f=> f!= Id);
+        public IEnumerable<PropertyInfo> AllNonId => All.Where(f => f != Id);
     }
 }
--- Mkb.DapperRepo/Reflection/ReflectionUtils.cs
+++ Mkb.DapperRepo/Reflection/ReflectionUtils.cs
@@ -1,32 +1,46 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Mkb.DapperRepo.Attributes;
 
 [assembly: InternalsVisibleTo("Mkb.DapperRepo.Tests")]
+
 namespace Mkb.DapperRepo.Reflection
 {
     internal static class ReflectionUtils
     {
+        private static Dictionary<Type, EntityPropertyInfo> TypeLookup = new Dictionary<Type, EntityPropertyInfo>();
+
         internal static EntityPropertyInfo GetEntityPropertyInfo<T>()
         {
+            if (TypeLookup.TryGetValue(typeof(T), out var item))
+            {
+                return item;
+            }
+
             var properties = typeof(T).GetProperties().ToArray();
-            var id = properties.FirstOrDefault(f => f.GetCustomAttributes(typeof(PrimaryKeyAttribute), false).Any()); // primary key determined by attribute now
-            return new EntityPropertyInfo(id, properties);
+            var id = properties.FirstOrDefault(f =>
+                f.GetCustomAttributes(typeof(PrimaryKeyAttribute), false)
+                    .Any()); // primary key determined by attribute now
+            var epv = new EntityPropertyInfo(id, properties);
+            TypeLookup.Add(typeof(T), epv);
+            return epv;
         }
 
-        internal static PropertyInfo GetPropertyInfoOfType<T>(Type type,string property, bool throwIfNotFound =true)
+        internal static PropertyInfo GetPropertyInfoOfType<T>(Type type, string property, bool throwIfNotFound = true)
         {
             var fields = GetEntityPropertyInfo<T>();
-            var theField = fields.All.FirstOrDefault(f =>
-                f.Name.ToLower() == property.ToLower() && f.PropertyType ==type);
+            var theField = fields.NameLookUp(property, type);
             if (theField == null && throwIfNotFound)
             {
-                if (fields.All.Any(f => f.Name.ToLower() == property.ToLower()))
+                var match = fields.NameLookUp(property);
+                if (match != null && match.Any())
                 {
-                    throw new Exception($"Type Must Be {type.Name}"); 
+                    throw new Exception($"Type Must Be {type.Name}");
                 }
+
                 throw new Exception($"Property:{property} not found in Type:{typeof(T).Name}");
             }
 
