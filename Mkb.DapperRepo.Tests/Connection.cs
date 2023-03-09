102f6f360e67fcdc0510adb803445bfbb06a0067
mkbmain <michael.bourke@live.co.uk>
2023-03-09 12:28:39 +0000

    Test Runner config

--- Mkb.DapperRepo.Tests/Connection.cs
+++ Mkb.DapperRepo.Tests/Connection.cs
@@ -1,13 +1,20 @@
 namespace Mkb.DapperRepo.Tests
 {
     public static class Connection
-    { 
-        // public const string MasterConnectionString = "Server=localhost;Database=master;User Id=sa;Password=A1234567a;";  // SQL
-        //  public const string MasterConnectionString = "Server=localhost;Database=master;Uid=root;Pwd=A1234567a;"; // MYSQL
-        //  public const string MasterConnectionString = "Server=192.168.0.216;Database=master;Uid=sa;Pwd=A1234567a;"; // MYSQL
-        //  public const string MasterConnectionString = "User ID=postgres;Password=A1234567a;Host=localhost;Port=5432;Database=master;"; // Postgres
+    {
+#if MYSQL
+        public const string MasterConnectionString = "Server=localhost;Database=master;Uid=root;Pwd=A1234567a;"; // MYSQL
+        public static Enviroment SelectedEnvironment = Enviroment.MySql;
+#elif POSTGRES
+          public const string MasterConnectionString = "User ID=postgres;Password=A1234567a;Host=localhost;Port=5432;Database=master;"; // Postgres
+                public static Enviroment SelectedEnvironment = Enviroment.PostgreSQL;
+#elif SQLSERVER
+        public const string MasterConnectionString = "Server=localhost;Database=master;User Id=sa;Password=A1234567a;"; // SQL
+        public static Enviroment SelectedEnvironment = Enviroment.Sql;
+#else
         public const string MasterConnectionString = "Data Source=master.sqlite"; // Sqlite
         public static Enviroment SelectedEnvironment = Enviroment.Sqlite;
+#endif
     }
 
     public enum Enviroment
