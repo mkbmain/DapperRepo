ae1bcbbed5c00166f79accb5dc6526ac8463df1e
mkbmain <michael.bourke@live.co.uk>
2023-03-13 05:32:33 +0000

    ctrl k d

--- Mkb.DapperRepo/Attributes/SqlColumnNameAttribute.cs
+++ Mkb.DapperRepo/Attributes/SqlColumnNameAttribute.cs
@@ -1,9 +1,10 @@
 namespace Mkb.DapperRepo.Attributes
 {
-    [System.AttributeUsage(System.AttributeTargets.Property)]  
-    public class SqlColumnNameAttribute :System.Attribute  
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+    public class SqlColumnNameAttribute : System.Attribute
     {
         public string Name { get; }
+
         public SqlColumnNameAttribute(string name)
         {
             Name = name;
