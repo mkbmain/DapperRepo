51af505b1d7ecd61c74a7794463bc82d0e5ebc73
mkbmain <michael.bourke@live.co.uk>
2024-04-12 14:06:09 +0100

    Update tests to xunit and dapper to latest version (#37)

--- Mkb.DapperRepo.Tests/Tests/BaseTestClasses/BaseDbSetupTestClass.cs
+++ Mkb.DapperRepo.Tests/Tests/BaseTestClasses/BaseDbSetupTestClass.cs
@@ -1,42 +1,36 @@
 using System;
 using System.IO;
 using Mkb.DapperRepo.Tests.Utils;
-using NUnit.Framework;
+using Xunit;
 
 namespace Mkb.DapperRepo.Tests.Tests.BaseTestClasses
 {
-    public abstract class BaseDbSetupTestClass
+    public abstract class BaseDbSetupTestClass : IDisposable
     {
-        public BaseDbSetupTestClass()
-        {
-            _className = this.GetType().Name;
-        }
-
         private static string RandomChars => Guid.NewGuid().ToString("N")[..8];
         private string _dbName;
         private readonly string _className;
 
         protected string Connection => DapperRepo.Tests.Connection.MasterConnectionString.Replace("master", _dbName);
 
-        [SetUp]
-        public void Setup()
+        protected BaseDbSetupTestClass()
         {
+            _className = this.GetType().Name;
             _dbName = (_className + RandomChars).ToLower();
             var scriptLocation = PathBuilder.BuildSqlScriptLocation($"CreateDbWithTestTable.{DapperRepo.Tests.Connection.SelectedEnvironment.ToString()}");
-            
+
             var sql = File.ReadAllText(scriptLocation).Replace("PlaceHolderDbName", _dbName);
             bool first = true;
 
             foreach (var item in sql.Split("{WaitBlock}"))
             {
                 // this might be come a bottlekneck as we create a new connection for every command but hay its tests should not be doing anything massive
-               DataBaseScriptRunnerAndBuilder.ExecuteCommandNonQuery(first? DapperRepo.Tests.Connection.MasterConnectionString : Connection, item);
-               first = false;
+                DataBaseScriptRunnerAndBuilder.ExecuteCommandNonQuery(first ? DapperRepo.Tests.Connection.MasterConnectionString : Connection, item);
+                first = false;
             }
         }
-
-        [TearDown]
-        public void TearDown()
+        
+        public void Dispose()
         {
             DataBaseScriptRunnerAndBuilder.KillDb(DapperRepo.Tests.Connection.MasterConnectionString, _dbName);
         }
